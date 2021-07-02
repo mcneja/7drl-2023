b2e89a054ad530b0f8df4eccac614335ac7270d3
James McNeill <mcneja@gmail.com>
2021-07-02 16:55:53 -0700

    Simplify rendering to direct primitives rather than indexed

--- web/fmm.js
+++ web/fmm.js
@@ -2,8 +2,6 @@
 
 window.onload = main;
 
-const maxQuads = 4096;
-
 // Functions
 
 function main() {
@@ -23,29 +21,32 @@ function main() {
 
 function initGlResources(gl) {
 	const vsSource = `
-		attribute vec4 aVertexPosition;
+		attribute vec3 aVertexPosition;
 		
 		uniform mat4 uProjectionMatrix;
 
-		varying highp vec2 vTextureCoord;
+		varying highp float vTextureCoord;
 
 		void main() {
 			gl_Position = uProjectionMatrix * vec4(aVertexPosition.xy, 0, 1);
-			vTextureCoord = aVertexPosition.zw;
+			vTextureCoord = aVertexPosition.z;
 		}
 	`;
 
 	const fsSource = `
-		varying highp vec2 vTextureCoord;
+		varying highp float vTextureCoord;
 
 		uniform sampler2D uContour;
 
 		void main() {
-			gl_FragColor = texture2D(uContour, vTextureCoord);
+			gl_FragColor = texture2D(uContour, vec2(vTextureCoord, 0));
 		}
 	`;
 
 	const program = initShaderProgram(gl, vsSource, fsSource);
+
+	const gridSizeX = 16;
+	const gridSizeY = 16;
 
 	const glResources = {
 		program: program,
@@ -56,10 +57,9 @@ function initGlResources(gl) {
 			projectionMatrix: gl.getUniformLocation(program, 'uProjectionMatrix'),
 			uContour: gl.getUniformLocation(program, 'uContour'),
 		},
-		vertexPositions: new Float32Array(maxQuads * 16),
-		numQuads: 0,
-		vertexPositionsBuffer: gl.createBuffer(),
-		vertexIndices: createElementBuffer(gl),
+		gridSizeX: gridSizeX,
+		gridSizeY: gridSizeY,
+		vertexBuffer: createVertexBuffer(gl, gridSizeX, gridSizeY),
 		contourTexture: createStripeTexture(gl),
 		projectionMatrix: createProjectionMatrix(),
 	};
@@ -68,11 +68,9 @@ function initGlResources(gl) {
 	gl.enable(gl.BLEND);
 	gl.clearColor(0, 0, 0, 1.0);
 
-	gl.bindBuffer(gl.ARRAY_BUFFER, glResources.vertexPositionsBuffer);
-	gl.vertexAttribPointer(glResources.attribLocations.vertexPosition, 4, gl.FLOAT, false, 0, 0);
+	gl.bindBuffer(gl.ARRAY_BUFFER, glResources.vertexBuffer);
+	gl.vertexAttribPointer(glResources.attribLocations.vertexPosition, 3, gl.FLOAT, false, 0, 0);
 	gl.enableVertexAttribArray(glResources.attribLocations.vertexPosition);
-
-	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, glResources.vertexIndices);
 
 	gl.useProgram(glResources.program);
 
@@ -95,26 +93,38 @@ function createProjectionMatrix() {
 	return projectionMatrix;
 }
 
-function createElementBuffer(gl) {
+function createVertexBuffer(gl, sizeX, sizeY) {
+	const vertexInfo = createVertexInfo(sizeX, sizeY);
 
-	const indices = new Uint16Array(maxQuads * 6);
+	const vertexBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, vertexInfo, gl.STATIC_DRAW);
 
-	for (let i = 0; i < maxQuads; ++i) {
-		let j = 6*i;
-		let k = 4*i;
-		indices[j+0] = k+0;
-		indices[j+1] = k+1;
-		indices[j+2] = k+2;
-		indices[j+3] = k+2;
-		indices[j+4] = k+1;
-		indices[j+5] = k+3;
+	return vertexBuffer;
+}
+
+function createVertexInfo(sizeX, sizeY) {
+	const v = new Float32Array(3 * 6 * sizeX * sizeY);
+	let i = 0;
+
+	function makeVert(x, y) {
+		v[i++] = x;
+		v[i++] = y;
+		v[i++] = Math.sqrt(x*x + y*y);
 	}
 
-	const indexBuffer = gl.createBuffer();
-	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
-	
-	return indexBuffer;
+	for (let x = 0; x < sizeX; ++x) {
+		for (let y = 0; y < sizeY; ++y) {
+			makeVert(x, y);
+			makeVert(x+1, y);
+			makeVert(x, y+1);
+			makeVert(x, y+1);
+			makeVert(x+1, y);
+			makeVert(x+1, y+1);
+		}
+	}
+
+	return v;
 }
 
 function drawScreen(gl, glResources) {
@@ -123,15 +133,14 @@ function drawScreen(gl, glResources) {
 	const screenY = gl.canvas.clientHeight;
 	gl.viewport(0, 0, screenX, screenY);
 
-	glResources.projectionMatrix[0] = 2 / screenX;
-	glResources.projectionMatrix[5] = 2 / screenY;
+	glResources.projectionMatrix[0] = 2 / glResources.gridSizeX;
+	glResources.projectionMatrix[5] = 2 / glResources.gridSizeY;
 
 	gl.clear(gl.COLOR_BUFFER_BIT);
 
 	gl.uniformMatrix4fv(glResources.uniformLocations.projectionMatrix, false, glResources.projectionMatrix);
 
-	addQuad(gl, glResources, 0, 0, screenX, screenY, 0, 16, 16, 32);
-	renderQuads(gl, glResources);
+	gl.drawArrays(gl.TRIANGLES, 0, glResources.gridSizeX * glResources.gridSizeY * 6);
 }
 
 function resizeCanvasToDisplaySize(canvas) {
@@ -175,49 +184,6 @@ function loadShader(gl, type, source) {
 	return shader;
 }
 
-function renderQuads(gl, glResources) {
-	if (glResources.numQuads > 0) {
-		gl.bindBuffer(gl.ARRAY_BUFFER, glResources.vertexPositionsBuffer);
-		gl.bufferData(gl.ARRAY_BUFFER, glResources.vertexPositions, gl.DYNAMIC_DRAW);
-
-		gl.drawElements(gl.TRIANGLES, 6 * glResources.numQuads, gl.UNSIGNED_SHORT, 0);
-	}
-	glResources.numQuads = 0;
-}
-
-function addQuad(gl, glResources, x0, y0, x1, y1, s00, s10, s01, s11) {
-	if (glResources.numQuads >= maxQuads) {
-		// Buffer is full; render
-		renderQuads(gl, glResources);
-	}
-
-	// Append four vertices to the position/texcoord array
-
-	const i = glResources.numQuads * 16;
-
-	glResources.vertexPositions[i+0] = x0;
-	glResources.vertexPositions[i+1] = y0;
-	glResources.vertexPositions[i+2] = s00;
-	glResources.vertexPositions[i+3] = 0;
-
-	glResources.vertexPositions[i+4] = x1;
-	glResources.vertexPositions[i+5] = y0;
-	glResources.vertexPositions[i+6] = s10;
-	glResources.vertexPositions[i+7] = 0;
-
-	glResources.vertexPositions[i+8] = x0;
-	glResources.vertexPositions[i+9] = y1;
-	glResources.vertexPositions[i+10] = s01;
-	glResources.vertexPositions[i+11] = 0;
-
-	glResources.vertexPositions[i+12] = x1;
-	glResources.vertexPositions[i+13] = y1;
-	glResources.vertexPositions[i+14] = s11;
-	glResources.vertexPositions[i+15] = 0;
-
-	++glResources.numQuads;
-}
-
 function createStripeTexture(gl) {
 	const stripeImageWidth = 64;
 	const stripeImage = new Uint8Array(stripeImageWidth);
@@ -241,49 +207,4 @@ function createStripeTexture(gl) {
 	gl.generateMipmap(gl.TEXTURE_2D);
 
 	return texture;
-}
-
-function createDistanceTexture(gl) {
-	const textureSizeX = 64;
-	const textureSizeY = 64;
-	const textureImage = new Float32Array(textureSizeX * textureSizeY);
-	for (let x = 0; x < textureSizeX; ++x) {
-		for (let y = 0; y < textureSizeY; ++y) {
-			textureImage[y * textureSizeX + x] = Math.sqrt(x^2 + y^2);
-		}
-	}
-
-	const texture = gl.createTexture();
-	gl.bindTexture(gl.TEXTURE_2D, texture);
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP);
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP);
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-
-	const level = 0;
-	const internalFormat = gl.LUMINANCE;
-	const srcFormat = gl.LUMINANCE;
-	const srcType = gl.FLOAT;
-	gl.texImage2D(gl.TEXTURE_2D, level, internalFormat, textureSizeX, textureSizeY, 0, srcFormat, srcType, textureImage);
-
-	return texture;
-}
-
-function generateMap() {
-	const gridSizeX = 32;
-	const gridSizeY = 32;
-
-	const gridImage = new Float32Array(gridSizeX * gridSizeY);
-
-	for (let x = 0; x < gridSizeX; ++x) {
-		for (let y = 0; y < gridSizeY; ++y) {
-			gridImage[y * gridSizeX + x] = Math.random();
-		}
-	}
-
-	return {
-		sizeX: gridSizeX,
-		sizeY: gridSizeY,
-		data: gridImage,
-	}
 }
