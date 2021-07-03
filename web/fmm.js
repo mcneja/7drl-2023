083fb5acfefc638d71f25b6c1fa558521445b1b0
James McNeill <mcneja@gmail.com>
2021-07-03 11:30:29 -0700

    Render speed/cost field in addition to the distance contour field

--- web/fmm.js
+++ web/fmm.js
@@ -21,27 +21,36 @@ function main() {
 
 function initGlResources(gl) {
 	const vsSource = `
-		attribute vec2 vPosition;
-		attribute vec3 vDistance;
+		attribute vec3 vPosition;
+		attribute vec2 vDistance;
+		attribute vec2 vSpeed;
 		
 		uniform mat4 uProjectionMatrix;
 
-		varying highp vec3 fDistance;
+		varying highp float fYBlend;
+		varying highp vec2 fDistance;
+		varying highp vec2 fSpeed;
 
 		void main() {
-			gl_Position = uProjectionMatrix * vec4(vPosition, 0, 1);
+			gl_Position = uProjectionMatrix * vec4(vPosition.xy, 0, 1);
+			fYBlend = vPosition.z;
 			fDistance = vDistance;
+			fSpeed = vSpeed;
 		}
 	`;
 
 	const fsSource = `
-		varying highp vec3 fDistance;
+		varying highp float fYBlend;
+		varying highp vec2 fDistance;
+		varying highp vec2 fSpeed;
 
 		uniform sampler2D uContour;
 
 		void main() {
-			highp float distance = mix(fDistance.x, fDistance.y, fDistance.z);
-			gl_FragColor = texture2D(uContour, vec2(distance, 0));
+			highp float distance = mix(fDistance.x, fDistance.y, fYBlend);
+			highp float speed = mix(fSpeed.x, fSpeed.y, fYBlend);
+			highp float b = 0.5 * speed + 0.5;
+			gl_FragColor = texture2D(uContour, vec2(distance, 0)) * vec4(1, b, b, 1);
 		}
 	`;
 
@@ -55,6 +64,7 @@ function initGlResources(gl) {
 		attribLocations: {
 			vertexPosition: gl.getAttribLocation(program, 'vPosition'),
 			vertexDistance: gl.getAttribLocation(program, 'vDistance'),
+			vertexSpeed: gl.getAttribLocation(program, 'vSpeed'),
 		},
 		uniformLocations: {
 			projectionMatrix: gl.getUniformLocation(program, 'uProjectionMatrix'),
@@ -72,11 +82,13 @@ function initGlResources(gl) {
 	gl.clearColor(0, 0, 0, 1.0);
 
 	gl.bindBuffer(gl.ARRAY_BUFFER, glResources.vertexBuffer);
-	const stride = 20; // five 4-byte floats
-	gl.vertexAttribPointer(glResources.attribLocations.vertexPosition, 2, gl.FLOAT, false, stride, 0);
-	gl.vertexAttribPointer(glResources.attribLocations.vertexDistance, 3, gl.FLOAT, false, stride, 8);
+	const stride = 28; // seven 4-byte floats
+	gl.vertexAttribPointer(glResources.attribLocations.vertexPosition, 3, gl.FLOAT, false, stride, 0);
+	gl.vertexAttribPointer(glResources.attribLocations.vertexDistance, 2, gl.FLOAT, false, stride, 12);
+	gl.vertexAttribPointer(glResources.attribLocations.vertexSpeed, 2, gl.FLOAT, false, stride, 20);
 	gl.enableVertexAttribArray(glResources.attribLocations.vertexPosition);
 	gl.enableVertexAttribArray(glResources.attribLocations.vertexDistance);
+	gl.enableVertexAttribArray(glResources.attribLocations.vertexSpeed);
 
 	gl.useProgram(glResources.program);
 
@@ -110,19 +122,26 @@ function createVertexBuffer(gl, sizeX, sizeY) {
 }
 
 function createVertexInfo(sizeX, sizeY) {
-	const v = new Float32Array(5 * 6 * sizeX * sizeY);
+	const v = new Float32Array(7 * 6 * sizeX * sizeY);
 	let i = 0;
 
 	function distance(x, y) {
 		return Math.sqrt(x*x + y*y);
 	}
 
-	function makeVert(x, y, d0, d1, d2) {
+	function speed(x, y) {
+		let z = ((x & 7) == 0 || (y & 7) == 0) ? 0 : 1;
+		return z;
+	}
+
+	function makeVert(x, y, s, d0, d1, c0, c1) {
 		v[i++] = x;
 		v[i++] = y;
+		v[i++] = s;
 		v[i++] = d0;
 		v[i++] = d1;
-		v[i++] = d2;
+		v[i++] = c0;
+		v[i++] = c1;
 	}
 
 	for (let x = 0; x < sizeX; ++x) {
@@ -131,12 +150,16 @@ function createVertexInfo(sizeX, sizeY) {
 			const dist10 = distance(x+1, y);
 			const dist01 = distance(x, y+1);
 			const dist11 = distance(x+1, y+1);
-			makeVert(x, y, dist00, dist01, 0);
-			makeVert(x+1, y, dist10, dist11, 0);
-			makeVert(x, y+1, dist00, dist01, 1);
-			makeVert(x, y+1, dist00, dist01, 1);
-			makeVert(x+1, y, dist10, dist11, 0);
-			makeVert(x+1, y+1, dist10, dist11, 1);
+			const speed00 = speed(x, y);
+			const speed10 = speed(x+1, y);
+			const speed01 = speed(x, y+1);
+			const speed11 = speed(x+1, y+1);
+			makeVert(x, y, 0, dist00, dist01, speed00, speed01);
+			makeVert(x+1, y, 0, dist10, dist11, speed10, speed11);
+			makeVert(x, y+1, 1, dist00, dist01, speed00, speed01);
+			makeVert(x, y+1, 1, dist00, dist01, speed00, speed01);
+			makeVert(x+1, y, 0, dist10, dist11, speed10, speed11);
+			makeVert(x+1, y+1, 1, dist10, dist11, speed10, speed11);
 		}
 	}
 
