a21871893f8c0d5d8b100f387684e72c45426a81
James McNeill <mcneja@gmail.com>
2024-11-01 06:52:19 -0700

    Ctrl+R restarts daily game, if daily game is active

--- src/game.ts
+++ src/game.ts
@@ -58,6 +58,7 @@ const minZoomLevel: number = -4;
 const maxZoomLevel: number = 16;
 const damageDisplayDuration = 0.5;
 const deadNotification: string = 'New Game: Ctrl+R\nMenu: Esc/Slash';
+const deadDailyNotification: string = 'Retry: Ctrl+R\nMenu: Esc/Slash';
 
 function loadResourcesThenRun() {
     Promise.all([
@@ -224,9 +225,13 @@ function updateControllerState(state:State) {
             state.lootStolen += loot;
             postTurn(state);
         } else if (activated('forceRestart')) {
-            state.rng = new RNG();
-            state.dailyRun = null;
-            restartGame(state);
+            if (state.dailyRun) {
+                startDailyGame(state);
+            } else {
+                state.rng = new RNG();
+                state.dailyRun = null;
+                restartGame(state);
+            }
         } else if (activated('nextLevel')) {
             if (state.level < state.gameMapRoughPlans.length - 1) {
                 scoreCompletedLevel(state);
@@ -1106,7 +1111,7 @@ function tryPlayerMakeNoise(state: State) {
 
     // Can't move if you're dead
     if (player.health <= 0) {
-        state.popups.setNotification(deadNotification, state.player.pos);
+        showDeadNotification(state);
         return;
     }
 
@@ -1155,7 +1160,7 @@ function tryPlayerWait(state: State) {
 
     // Can't move if you're dead.
     if (player.health <= 0) {
-        state.popups.setNotification(deadNotification, state.player.pos);
+        showDeadNotification(state);
         return;
     }
 
@@ -1185,7 +1190,7 @@ function tryPlayerStep(state: State, dx: number, dy: number, stepType: StepType)
 
     const player = state.player;
     if (player.health <= 0) {
-        state.popups.setNotification(deadNotification, state.player.pos);
+        showDeadNotification(state);
         return;
     }
 
@@ -1603,6 +1608,10 @@ function showMoveTutorialNotifications(state: State, posPrev: vec2) {
     }
 }
 
+function showDeadNotification(state: State) {
+    state.popups.setNotification(state.dailyRun ? deadDailyNotification : deadNotification, state.player.pos);
+}
+
 function isAnyoneAwareOfPlayer(state: State): boolean {
     return state.gameMap.guards.some(guard => !isRelaxedGuardMode(guard.mode) && guard.mode !== GuardMode.Unconscious);
 }
@@ -1722,7 +1731,7 @@ function tryPlayerLeap(state: State, dx: number, dy: number) {
 
     const player = state.player;
     if (player.health <= 0) {
-        state.popups.setNotification(deadNotification, state.player.pos);
+        showDeadNotification(state);
         return;
     }
 
@@ -3159,6 +3168,13 @@ export function restartGame(state: State) {
 //    analyzeLevel(state);
     Howler.stop();
     playAmbience(state);
+}
+
+export function startDailyGame(state: State) {
+    let date = getCurrentDateFormatted();
+    state.rng = new RNG('Daily '+date);
+    state.dailyRun = date;
+    restartGame(state);
 }
 
 function resetState(state: State) {
